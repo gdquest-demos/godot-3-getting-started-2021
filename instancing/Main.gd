922fb50db07b5320412e4e73194f20b9d8aae4ec
Nathan Lovato <nathan@gdquest.com>
2021-03-20 06:40:58 -0600

    Add a copy of the instancing demo

---
+++ instancing/Main.gd
@@ -0,0 +1,9 @@
+extends Node
+
+export (PackedScene) var Ball
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		var new_ball = Ball.instance()
+		new_ball.position = get_viewport().get_mouse_position()
+		add_child(new_ball)
