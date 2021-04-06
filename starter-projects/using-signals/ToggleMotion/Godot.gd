21f5c1070b4fe76c7f65c042ef91e012217ac516
Nathan Lovato <nathan@gdquest.com>
2021-04-06 17:21:35 -0600

    Remove extra signal connection and callback in signal start project

--- starter-projects/using-signals/ToggleMotion/Godot.gd
+++ starter-projects/using-signals/ToggleMotion/Godot.gd
@@ -8,11 +8,3 @@ func _process(delta):
 	rotation += angular_speed * delta
 	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
-
-
-func _on_Button_pressed():
-	set_process(not is_processing())
-
-
-func _on_Timer_timeout():
-	visible = not visible
