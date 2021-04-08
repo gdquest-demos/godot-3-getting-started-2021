f64c00779514d62862ad26dbef9bdb62fd5b8fb3
Nathan Lovato <nathan@gdquest.com>
2021-04-08 08:17:16 -0600

    Add a copy of Dodge the creeps, with a starter project

---
+++ 2d-dodge-the-creeps/Main.gd
@@ -0,0 +1,54 @@
+extends Node
+
+export (PackedScene) var mob_scene
+var score
+
+
+func _ready():
+	randomize()
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
+
+
+func new_game():
+	get_tree().call_group("mobs", "queue_free")
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	$Music.play()
+
+
+func _on_MobTimer_timeout():
+	var mob_spawn_location = get_node("MobPath/MobSpawnLocation")
+	mob_spawn_location.offset = randi()
+
+	var mob = mob_scene.instance()
+	add_child(mob)
+
+	var direction = mob_spawn_location.rotation + PI / 2
+
+	mob.position = mob_spawn_location.position
+
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+
+	var velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = velocity.rotated(direction)
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
---
+++ 2d-dodge-the-creeps/Mob.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+export var min_speed = 150
+export var max_speed = 250
+
+
+func _ready():
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_start_game():
+	queue_free()
